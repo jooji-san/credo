f44d38f5118934968e3344cc036c59d8498b20eb
Giorgi Shengelaia <g.shengelaia2001@gmail.com>
2021-12-28 16:47:05 +0400

    first

---
+++ list/list/List.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace list
+{
+    class MyList<T>
+    {
+        public int Count { get; private set; }
+        public int Capacity { get; private set; }
+        private T[] arr = new T[0];
+
+        private void Copy(T[] newArr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                newArr[i] = arr[i];
+            }
+        }
+        private void Grow(int newCapacity)
+        {
+            T[] tmp = new T[newCapacity];
+            Copy(tmp);
+            arr = tmp;
+            Capacity = newCapacity;
+        }
+        public void Add(T item)
+        {
+            if (Count == Capacity)
+            {
+                // grow capacity 20 items at a time
+                // to reserve space for future potential Adds
+                Grow(Capacity + 20);
+            }
+
+            arr[Count] = item;
+            Count++;
+        }
+
+        private void ShiftLeft(int index)
+        {
+            for (int i = index; i < Count - 1; i++)
+            {
+                arr[i] = arr[i + 1];
+            }
+        }
+        public void Remove(int index)
+        {
+            if (index > Count - 1 || index < 0) return;
+
+            ShiftLeft(index);
+            Count--;
+        }
+        public T Get(int index)
+        {
+            if (index > Count - 1 || index < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            return arr[index];
+        }
+
+
+    }
+}
---
+++ list/list/Program.cs
@@ -0,0 +1,11 @@
+﻿using list;
+
+MyList<int> l = new();
+l.Add(1);
+l.Add(2);
+l.Add(3);
+Console.WriteLine("at zero index: " + l.Get(0));
+Console.WriteLine("count: " + l.Count);
+l.Remove(0);
+Console.WriteLine("at zero index: " + l.Get(0));
+Console.WriteLine("count: " + l.Count);
